88e0d5fdb701188cce2ab68fb266aeb545a1f830
dgk <276877056@qq.com>
2020-04-30 23:27:08 +0800

    1.1 1.2内容完成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import threeHelper from './lib/threeHelper';
+
 
 function App() {
+  useEffect(() => {
+    threeHelper();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }

1569ea2baf66cae14f27f2a0558e57f1eec69c05
dgk <276877056@qq.com>
2020-05-11 11:44:40 +0800

    no message

--- src/lib/threeHelper.js
+++ src/lib/threeHelper.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { TrackballControls } from 'three-trackballcontrols-ts';
+const OrbitControls = require('three-orbit-controls')(THREE);
 
 class GetInterval {
     T0 = new Date();
@@ -21,13 +23,14 @@ class ThreeHelper {
         this.addAmbientLignt();
         this.addPointLignt();
 
-        this.addMesh();
-        this.setCamera();
+        this.addBoxGeometry();
+        this.addCylinderGeometry();
+        this.addCamera();
         this.addRender();
+        this.addControls();
+        this.addAxisHelper();
 
         this.animate();
-
-
     }
 
     addAmbientLignt() {
@@ -41,7 +44,7 @@ class ThreeHelper {
         this.scene.add(point); //点光源添加到场景中
     };
 
-    addMesh() {
+    addBoxGeometry() {
         /**
          * 创建网格模型
          */
@@ -54,19 +57,46 @@ class ThreeHelper {
         this.scene.add(mesh); //网格模型添加到场景中
         this.mesh = mesh;
     }
+    addCylinderGeometry() {
+        const geometry = new THREE.CylinderGeometry(50, 50, 100, 25);
+        const meterial = new THREE.MeshLambertMaterial({
+            color: 0xff0000,
+            opacity: 0.7,
+            transparent: true,
+            // wireframe: true
+        });
+        const meterialPhong = new THREE.MeshPhongMaterial({
+            color: 0x0000ff,
+            specular: 0x4488ee,
+            shininess: true
+        });
+        const mesh = new THREE.Mesh(geometry, meterialPhong);
+        mesh.position.x += 200;
+        this.scene.add(mesh);
+    }
 
-    setCamera() {
+    addAxisHelper() {
+        const axisHelper = new THREE.AxisHelper(250);
+        this.scene.add(axisHelper);
+    }
+
+    addCamera() {
+        // /**
+        //  * 投影相机设置
+        //  */
+        // let k = this.width / this.height; //窗口宽高比
+        // let s = 200; //三维场景显示范围控制系数，系数越大，显示的范围越大
+        // //创建相机对象
+        // this.camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+        // // var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+        // this.camera.position.set(200, 500, 200); //设置相机位置
+        // this.camera.lookAt(this.scene.position); //设置相机方向(指向的场景对象)
         /**
-         * 相机设置
+         * 透视相机设置
          */
-
-        let k = this.width / this.height; //窗口宽高比
-        let s = 200; //三维场景显示范围控制系数，系数越大，显示的范围越大
-        //创建相机对象
-        this.camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-        // var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
-        this.camera.position.set(200, 500, 200); //设置相机位置
-        this.camera.lookAt(this.scene.position); //设置相机方向(指向的场景对象)
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        this.camera.position.set(10, 10, 25);
     }
 
 
@@ -80,6 +110,14 @@ class ThreeHelper {
         this.ele.appendChild(this.renderer.domElement); //body元素中插入canvas对象
         //执行渲染操作   指定场景、相机作为参数
         this.renderer.render(this.scene, this.camera);
+
+
+    }
+    addControls() {
+        this.controls = new OrbitControls(this.camera);
+        // this.controls = new TrackballControls(this.camera, this.renderer.domElement);
+        this.controls.minDistance = 210;
+        this.controls.maxDistance = 1000;
     }
 
 
@@ -87,10 +125,11 @@ class ThreeHelper {
     interval = new GetInterval();
     // 渲染函数
     animate() {
+        this.controls.update();
         requestAnimationFrame(() => this.animate());//请求再次执行渲染函数render
         this.renderer.render(this.scene, this.camera);//执行渲染操作
-        this.mesh.rotateY(0.001 * this.interval.get());//每次绕y轴旋转0.01弧度
-        this.mesh.rotation.x += 0.01;
+        // this.mesh.rotateY(0.001 * this.interval.get());//每次绕y轴旋转0.01弧度
+        // this.mesh.rotation.x += 0.01;
     }
 
 }
